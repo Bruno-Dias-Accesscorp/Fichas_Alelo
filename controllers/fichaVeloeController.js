bfbf5c3e0feb76017296aa1dc6f41ae7eff45522
Bruno Dias de Deus <bruno.deus@accesscorp.com.br>
2022-04-11 17:00:15 -0300

    Gerando data

--- controllers/fichaVeloeController.js
+++ controllers/fichaVeloeController.js
@@ -1,3 +1,4 @@
+const { time } = require("console");
 const fs = require("fs")
 const veloe_campos = require("../JSON/campos_veloe");
 
@@ -8,16 +9,43 @@ module.exports = {
     },
 
     async criar(req, res, next){
-        let data = { ...req.body }
+        let campos = { ...req.body }
         
-        let valor = veloe_campos[0].valor + "\r\n" + "OK"
+        let id_001 = veloe_campos[0].valor 
+
+        // + "\r\n" + "OK"
+
+        var data = new Date()
+        var dia
+        var mes
+        var ano = data.getFullYear()
+
+            if(data.getDate < 10){
+                var diaTexto = data.getDate()
+                dia = "0" + diaTexto.toString()
+            }else{
+                var diaTexto = data.getDate()
+                dia = diaTexto.toString()
+            }
+
+            if(data.getMonth() < 10){
+                var mesTexto = data.getMonth()
+                mes = "0" + mesTexto.toString()
+            }else{
+                var mesTexto = data.getMonth()
+                mes = mesTexto.toString()
+            }
+
+        let id_002 = dia + mes + ano
+        let id_003 = () => {fs.readdir('./Veloe', (err, paths) => {return paths})}
+
+        console.log(id_003())
         
         
-        
-        fs.writeFile('Veloe\\arquivo.txt', valor, (err)=> {
-            if (err) throw err;
-            console.log("O arquivo foi criado!");
-        })
-            res.render("veloe")
+        // fs.writeFile('Veloe\\arquivo.txt', valor, (err)=> {
+        //     if (err) throw err;
+        //     console.log("O arquivo foi criado!");
+        // })
+        //     res.render("veloe")
     },
 }
