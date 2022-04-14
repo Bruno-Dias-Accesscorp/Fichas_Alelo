c31e0d10216d5ebe5a2678f6e965dc3b17312876
Bruno Dias de Deus <bruno.deus@accesscorp.com.br>
2022-04-14 17:45:35 -0300

    adicionando ultimos campos d1

--- controllers/fichaVeloeController.js
+++ controllers/fichaVeloeController.js
@@ -1,7 +1,8 @@
 const { time } = require("console");
 const fs = require("fs")
 const veloe_campos = require("../JSON/campos_veloe");
-
+const zeros = "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+const espacos = "                                                                                                                                                                                                                                                                                                             "
 
 module.exports = {
     async veloe(req, res){
@@ -71,33 +72,95 @@ module.exports = {
         // let cnpjFilial = id_006_1.substr(8,4)
         // let cnpjDigito = id_006_1.substr(12,2)
         let id_006 = campos.CNPJ.replace(/[^\d]+/g,'')
-        let id_007 = campos.NOME_FANTASIA
+        
         let id_008 = campos.RAZAO_SOCIAL
         
-        let id_009_1 = campos.TIPO_EMPRESA
+        let id_007 = campos.NOME_FANTASIA
 
-        if(id_009_1 == "Tipo de Empresa..."){
-            id_009_1 = "000"
+        if(id_007 == ""){
+            id_007 = id_008.substring(0,)
+        }
+
+        let id_009 = campos.TIPO_EMPRESA
+
+        if(id_009 == "Tipo de Empresa..."){
+            id_009 = "000"
         }else{
-        if(id_009_1 < 10){
-            "00" + id_009_1 
+        if(id_009 < 10){
+            id_009 = "00" + id_009 
+            
         }else{
-            "0" + id_009_1 
+            id_009 = "0" + id_009 
         }
         }
 
-        let id_009 = id_009_1
+        let id_010 = veloe_campos[3].valor 
 
-        let gerarLinha_2 = id_005 + id_006 + id_007 + id_008 + id_009
+        let id_011 = campos.QUANTIDADE_TOTAL_VEICULOS
+        
+        if(id_011 == ""){
+            id_011 = zeros.substring(0,6)
+        }else{
+        if(id_011 < 10){ id_011 = zeros.substring(0,5) + id_011 }else{
+        if(id_011 < 100){ id_011 = zeros.substring(0,4) + id_011 }else{
+        if(id_011 < 1000){ id_011 = zeros.substring(0,3) + id_011 }else{
+        if(id_011 < 10000){ id_011 = zeros.substring(0,2) + id_011 }else{
+        if(id_011 < 100000){ id_011 = zeros.substring(0,1) + id_011 }
+        }}}}}
 
-        console.log(id_008)
+        let id_012 = campos.FATURAMENTO_MEDIO_MENSAL
+
+        if(id_012 == ""){id_012 = zeros.substring(0,13) }else{
+        if(id_012 < 10){ id_012 = zeros.substring(0,12) + id_012 }else{
+        if(id_012 < 100){ id_012 = zeros.substring(0,11) + id_012 }else{
+        if(id_012 < 1000){ id_012 = zeros.substring(0,10) + id_012 }else{
+        if(id_012 < 10000){ id_012 = zeros.substring(0,9) + id_012 }else{
+        if(id_012 < 100000){ id_012 = zeros.substring(0,8) + id_012 }else{
+        if(id_012 < 1000000){ id_012 = zeros.substring(0,7) + id_012 }else{
+        if(id_012 < 10000000){ id_012 = zeros.substring(0,6) + id_012 }else{
+        if(id_012 < 100000000){ id_012 = zeros.substring(0,5) + id_012 }else{
+        if(id_012 < 1000000000){ id_012 = zeros.substring(0,4) + id_012 }else{
+        if(id_012 < 10000000000){ id_012 = zeros.substring(0,3) + id_012 }else{
+        if(id_012 < 100000000000){ id_012 = zeros.substring(0,2) + id_012 }else{
+        if(id_012 < 1000000000000){ id_012 = zeros.substring(0,1) + id_012 }
+        }}}}}}}}}}}}
 
         
 
-        // fs.writeFile('Veloe\\TERADE_' + id_002 + "_" + id_003 + ".txt", gerarLinha_1 + gerarLinha_2, (err)=> {
-        //     if (err) throw err;
-        //     console.log("O arquivo foi criado!");
-        // })
+        let id_016 = campos.INSCRICAO_MUNICIPAL
+
+        if(id_016 == ""){id_016 = zeros.substring(0,20) }else{
+            if(id_016 < 10){ id_016 = zeros.substring(0,19) + id_016 }else{
+            if(id_016 < 100){ id_016 = zeros.substring(0,18) + id_016 }else{
+            if(id_016 < 1000){ id_016 = zeros.substring(0,17) + id_016 }else{
+            if(id_016 < 10000){ id_016 = zeros.substring(0,16) + id_016 }else{
+            if(id_016 < 100000){ id_016 = zeros.substring(0,15) + id_016 }else{
+            if(id_016 < 1000000){ id_016 = zeros.substring(0,14) + id_016 }else{
+            if(id_016 < 10000000){ id_016 = zeros.substring(0,13) + id_016 }else{
+            if(id_016 < 100000000){ id_016 = zeros.substring(0,12) + id_016 }else{
+            if(id_016 < 1000000000){ id_016 = zeros.substring(0,11) + id_016 }else{
+            if(id_016 < 10000000000){ id_016 = zeros.substring(0,10) + id_016 }else{
+            if(id_016 < 100000000000){ id_016 = zeros.substring(0,9) + id_016 }else{
+            if(id_016 < 1000000000000){ id_016 = zeros.substring(0,8) + id_016 }else{
+            if(id_016 < 10000000000000){ id_016 = zeros.substring(0,7) + id_016 }else{
+            if(id_016 < 100000000000000){ id_016 = zeros.substring(0,6) + id_016 }else{
+            if(id_016 < 1000000000000000){ id_016 = zeros.substring(0,5) + id_016 }else{
+            if(id_016 < 10000000000000000){ id_016 = zeros.substring(0,4) + id_016 }else{
+            if(id_016 < 100000000000000000){ id_016 = zeros.substring(0,3) + id_016 }else{
+            if(id_016 < 1000000000000000000){ id_016 = zeros.substring(0,2) + id_016 }else{
+            if(id_016 < 10000000000000000000){ id_016 = zeros.substring(0,1) + id_016 }
+            }}}}}}}}}}}}}}}}}}}
+
+            let id_017 = veloe_campos[4].valor
+
+            
+
+        let gerarLinha_2 = id_005 + id_006 + id_007 + id_008 + id_009 + id_010 + id_011 + id_012 + id_013 + id_014 + id_015 + "\r\n"
+
+        fs.writeFile('Veloe\\TERADE_' + id_002 + "_" + id_003 + ".txt", gerarLinha_1 + gerarLinha_2 + gerarLinha_3, (err)=> {
+            if (err) throw err;
+            console.log("O arquivo foi criado!");
+        })
             res.render("veloe")
     },
 }
